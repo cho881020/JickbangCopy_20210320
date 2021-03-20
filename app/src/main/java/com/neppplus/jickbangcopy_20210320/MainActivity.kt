2fa26092129a994469b0a9371bd8a881d6e0aecc
코딩티처지니 <kj_cho@idealidea.co.kr>
2021-03-20 15:03:10 +0900

    실제 방 데이터를 목록에 추가

--- app/src/main/java/com/neppplus/jickbangcopy_20210320/MainActivity.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20210320/MainActivity.kt
@@ -2,10 +2,29 @@ package com.neppplus.jickbangcopy_20210320
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.jickbangcopy_20210320.datas.Room
 
 class MainActivity : AppCompatActivity() {
+
+    val mRooms = ArrayList<Room>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mRooms.add(  Room(7500, "서울시 동대문구", 2, "동대문구의 방입니다.")  )
+        mRooms.add(  Room(18500, "서울시 동대문구", 5, "동대문구의 방입니다.")  )
+        mRooms.add(  Room(25000, "서울시 동대문구", 0, "동대문구의 방입니다.")  )
+        mRooms.add(  Room(3400, "서울시 동대문구", -3, "동대문구의 방입니다.")  )
+        mRooms.add(  Room(9500, "서울시 서대문구", 8, "서대문구의 방입니다.")  )
+        mRooms.add(  Room(156000, "서울시 서대문구", 6, "서대문구의 방입니다.")  )
+        mRooms.add(  Room(78000, "서울시 서대문구", 0, "서대문구의 방입니다.")  )
+        mRooms.add(  Room(96000, "서울시 서대문구", -2, "서대문구의 방입니다.")  )
+        mRooms.add(  Room(2300, "서울시 중구", -1, "중구의 방입니다.")  )
+        mRooms.add(  Room(4000, "서울시 중구", 15, "중구의 방입니다.")  )
+        mRooms.add(  Room(8500, "서울시 중구", 20, "중구의 방입니다.")  )
+        mRooms.add(  Room(680000, "서울시 중구", -2, "중구의 방입니다.")  )
+
+
     }
 }
