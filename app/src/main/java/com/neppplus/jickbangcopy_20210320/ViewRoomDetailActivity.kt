f9b20d2ff7e628ecfec65e35684e414c8c861174
코딩티처지니 <kj_cho@idealidea.co.kr>
2021-03-20 16:01:47 +0900

    Room클래스를 직렬화(Serializable) 해서 통째로 첨부 가능하도록.

--- app/src/main/java/com/neppplus/jickbangcopy_20210320/ViewRoomDetailActivity.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20210320/ViewRoomDetailActivity.kt
@@ -7,5 +7,8 @@ class ViewRoomDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_room_detail)
+
+
+
     }
 }
